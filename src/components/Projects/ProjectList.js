c07d5b74d739931d2be9e4f55a01f7579a907487
Jordan Sherman <jordandsherman@gmail.com>
2019-04-29 23:39:18 -0400

    Populated ProjectSummary component and using in ProjectList.

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,43 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const projectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken">
-                    <span className="card-title">
-                        Project Title 1
-                    </span>
-                    <p>Posted by Jordan</p>
-                    <p className="grey-text">
-                        April 29th, 2019
-                    </p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken">
-                    <span className="card-title">
-                        Project Title 2
-                    </span>
-                    <p>Posted by Jordan</p>
-                    <p className="grey-text">
-                        April 29th, 2019
-                    </p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken">
-                    <span className="card-title">
-                        Project Title 3
-                    </span>
-                    <p>Posted by Jordan</p>
-                    <p className="grey-text">
-                        April 29th, 2019
-                    </p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ src/components/Projects/ProjectSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const projectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken">
+                <span className="card-title">
+                    Project Title
+                    </span>
+                <p>Posted by Jordan</p>
+                <p className="grey-text">
+                    April 29th, 2019
+                    </p>
+            </div>
+        </div>
+    )
+}
+
+export default projectSummary;
