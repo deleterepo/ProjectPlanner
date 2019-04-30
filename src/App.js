83a91943a25fdf1851c709a68f7945c383bb69f3
Jordan Sherman <jordandsherman@gmail.com>
2019-04-29 23:46:55 -0400

    Populated ProjectDetails component and added route for it.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Dashboard from './components/Dashboard/Dashboard';
+import ProjectDetails from './components/Projects/ProjectDetails';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/Projects/ProjectDetails.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">
+                        Project Title
+                </span>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto laboriosam, quaerat placeat impedit possimus nam officia natus ut! Repellat ipsa sequi similique eaque rem aspernatur, quos iure laboriosam explicabo accusantium.</p>
+                </div>
+                <div className="card-action-grey lighten-4 grey-text">
+                    <div>Posted by Jordan</div>
+                    <div>April 29th, 2019</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectDetails
