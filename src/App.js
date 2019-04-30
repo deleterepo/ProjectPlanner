15eb7b32c9a26a1fbd1af87e0dc73b354123affa
Jordan Sherman <jordandsherman@gmail.com>
2019-04-29 23:33:06 -0400

    Added route for Dashboard component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -41,3 +41,5 @@ const projectList = () => {
         </div>
     )
 }
+
+export default projectList;
