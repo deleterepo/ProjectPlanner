9fa4557ec4a558763707f6bdb4010c5d85e17b8a
Jordan Sherman <jordandsherman@gmail.com>
2019-04-29 23:31:00 -0400

    Populated Dashboard, Notifications and ProjectList components.

---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const projectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken">
+                    <span className="card-title">
+                        Project Title 1
+                    </span>
+                    <p>Posted by Jordan</p>
+                    <p className="grey-text">
+                        April 29th, 2019
+                    </p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken">
+                    <span className="card-title">
+                        Project Title 2
+                    </span>
+                    <p>Posted by Jordan</p>
+                    <p className="grey-text">
+                        April 29th, 2019
+                    </p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken">
+                    <span className="card-title">
+                        Project Title 3
+                    </span>
+                    <p>Posted by Jordan</p>
+                    <p className="grey-text">
+                        April 29th, 2019
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
