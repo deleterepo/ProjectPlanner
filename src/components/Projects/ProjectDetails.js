51d67e36ca8d54841b4e3ae251718f506a7e7d24
Jordan Sherman <jordandsherman@gmail.com>
2019-04-29 23:52:48 -0400

    Passing in props to ProjectDetails and displaying project id.

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
+
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
                     <span className="card-title">
-                        Project Title
+                        Project Title - {id}
                     </span>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto laboriosam, quaerat placeat impedit possimus nam officia natus ut! Repellat ipsa sequi similique eaque rem aspernatur, quos iure laboriosam explicabo accusantium.</p>
                 </div>
