7a885d8500372332da0f834b1f910b23e8ac4483
Jordan Sherman <jordandsherman@gmail.com>
2019-05-04 16:05:10 -0400

    Including firebaseConfig in another file for easier deployment to different environments.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,7 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDRcXq8418ULAFhZ1xBYO5pqBiu_LF6qG8",
-    authDomain: "project-planner-c1384.firebaseapp.com",
-    databaseURL: "https://project-planner-c1384.firebaseio.com",
-    projectId: "project-planner-c1384",
-    storageBucket: "project-planner-c1384.appspot.com",
-    messagingSenderId: "122629424668",
-    appId: "1:122629424668:web:0f6e607bcfdefb55"
-  };
+import firebaseConfig from './firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 
