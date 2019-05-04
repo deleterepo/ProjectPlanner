cb20e8fb82f82471c6abb97cdcaca7602bc5db29
Jordan Sherman <jordandsherman@gmail.com>
2019-05-04 14:59:38 -0400

    Added store enhancers for firebase and firestore. Composing with thunk middleware.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/RootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import firebase from './firebase/firebase'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase),
+    reactReduxFirebase(firebase)
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}>
--- src/store/actions/ProjectActions.js
+++ src/store/actions/ProjectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to db
         console.log('dispatched CREATE_PROJECT');
         dispatch({ type: 'CREATE_PROJECT', project });
