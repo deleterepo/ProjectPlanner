483988dc25a0f7e24a0b44e591482c1610e7d4a8
Jordan Sherman <jordandsherman@gmail.com>
2019-05-26 22:24:41 -0400

    Linking Project to ProjectDetails on ProjectList component. Using data from firestore in ProjectDetails.

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -1,25 +1,51 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id;
+    const { project } = props;
+    if (project) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">
+                            {project.title}
+                        </span>
+                        <p>{project.content}</p>
+                    </div>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>Posted by {project.authorFistName} {project.authorLastName}</div>
+                        <div>April 29th, 2019</div>
+                    </div>
+                </div>
 
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">
-                        Project Title - {id}
-                    </span>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto laboriosam, quaerat placeat impedit possimus nam officia natus ut! Repellat ipsa sequi similique eaque rem aspernatur, quos iure laboriosam explicabo accusantium.</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Jordan</div>
-                    <div>April 29th, 2019</div>
-                </div>
             </div>
+        )
 
-        </div>
-    )
+    } else {
+        return (
+            <div className="container center"><p>Loading project...</p></div>
+        )
+    }
+
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    console.log(state);
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null;
+
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(ProjectDetails)
