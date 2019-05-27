27416f4f1cedc82f7523c23b68cd89ec0272f1b1
Jordan Sherman <jordandsherman@gmail.com>
2019-05-26 22:25:40 -0400

    Cleanup.

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -7,23 +7,21 @@ const ProjectDetails = (props) => {
     const { project } = props;
     if (project) {
         return (
-            <div className="container section project-details">
-                <div className="card z-depth-0">
-                    <div className="card-content">
-                        <span className="card-title">
-                            {project.title}
-                        </span>
-                        <p>{project.content}</p>
-                    </div>
-                    <div className="card-action grey lighten-4 grey-text">
-                        <div>Posted by {project.authorFistName} {project.authorLastName}</div>
-                        <div>April 29th, 2019</div>
-                    </div>
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">
+                        {project.title}
+                    </span>
+                    <p>{project.content}</p>
                 </div>
-
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by {project.authorFistName} {project.authorLastName}</div>
+                    <div>April 29th, 2019</div>
+                </div>
             </div>
+        </div>
         )
-
     } else {
         return (
             <div className="container center"><p>Loading project...</p></div>
