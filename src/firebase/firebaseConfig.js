85e73f9941a097d15b353239bd66019d73589009
Jordan Sherman <jordandsherman@gmail.com>
2019-07-06 18:35:55 -0400

    Including firebaseConfig (safe as apiKey is public).

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export default {
+    apiKey: "AIzaSyDRcXq8418ULAFhZ1xBYO5pqBiu_LF6qG8",
+    authDomain: "project-planner-c1384.firebaseapp.com",
+    databaseURL: "https://project-planner-c1384.firebaseio.com",
+    projectId: "project-planner-c1384",
+    storageBucket: "project-planner-c1384.appspot.com",
+    messagingSenderId: "122629424668",
+    appId: "1:122629424668:web:0f6e607bcfdefb55"
+  };
