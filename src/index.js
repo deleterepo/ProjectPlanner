9c888693a3dc1e4a1036c1acbc9763cb554b6f10
Jordan Sherman <jordandsherman@gmail.com>
2019-07-06 17:24:44 -0400

    Waiting until auth status is retrieved to render app.

--- src/index.js
+++ src/index.js
@@ -14,16 +14,17 @@ import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 const store = createStore(rootReducer, compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(firebase),
-    reactReduxFirebase(firebase)
-    )
+    reactReduxFirebase(firebase, { attachAuthIsReady: true })
+)
 );
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
-
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+        , document.getElementById('root'));
+})
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
